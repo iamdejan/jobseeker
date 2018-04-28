038a532f74a3e5b7910483d0aac6f166702e66bf
iamdejan <15686093+iamdejan@users.noreply.github.com>
2018-04-28 14:13:22 +0700

    Add files to upload
    Edit revision

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JobSurfer
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <!-- <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
+
+                    <a>Giovanni Dejan</a>
+                    <a>Byran Karunachandra</a>
+                    <a>Teguh Wibowo Wijaya</a>
+                    <a>Vincent Tansol</a>
                 </div>
             </div>
         </div>
